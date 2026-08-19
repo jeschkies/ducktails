1007ea37520ed8c91158a414a727613b913c8755
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2026-08-19 16:45:58 +0200

    Accept query

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@ mod error;
 mod parser;
 mod scanner;
 
+use crate::parser::Parser;
+use clap::Parser as ClapParser;
+
+/// Simple program to greet a person
+#[derive(ClapParser, Debug)]
+#[command(version, about, long_about = None)]
+struct CLI {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    query: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CLI::parse();
+
+    let expr = Parser::parse(args.query.as_str()).unwrap();
+    println!("{:?}", expr)
 }
